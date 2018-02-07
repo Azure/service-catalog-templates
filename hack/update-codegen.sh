3048d75a9e34d0fa0e94508f7d253b7b9a1068e2
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-02-07 13:19:52 -0600

    fix codegen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -28,7 +28,5 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/Azure/service-catalog-templates/pkg/client github.com/Azure/service-catalog-templates/pkg/apis \
   templatescontroller:experimental \
-  --output-base "$(dirname ${BASH_SOURCE})/../../.."
-
-# To use your own boilerplate text append:
-#   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
+  --output-base "$(dirname ${BASH_SOURCE})/../../.." \
+  --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
