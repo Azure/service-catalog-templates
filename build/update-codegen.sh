342c025ea15ec7069c52300852eea65f6fc40711
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-03-03 05:24:23 -0600

    Fix codegen case problem
    One of the codegen steps is outputting to a lower-case azure
     directory and writing incorrect import paths

--- build/update-codegen.sh
+++ build/update-codegen.sh
@@ -14,14 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
 set -o errexit
 set -o nounset
 set -o pipefail
 
-rm -fr ./pkg/client
-
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+SCRIPT_ROOT=$(realpath $(dirname ${BASH_SOURCE})/..)
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+echo "Generating code in ${SCRIPT_ROOT}"
+rm -fr ${SCRIPT_ROOT}/pkg/client
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
@@ -32,6 +34,9 @@ ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   templates:experimental \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
 
-# Fix casing problem... not sure why Azure is being turned into azure
-
-find ./pkg/client -type f -exec sed -i 's/azure/Azure/g' {} \;
+# Fix casing problem, where one directory is generated into a directory under lower-case "azure"
+DEST_DIR=${SCRIPT_ROOT}/pkg/client/clientset/versioned/typed/templates/
+SRC_DIR=${SCRIPT_ROOT/Azure/azure}/pkg/client/clientset/versioned/typed/templates/experimental
+mkdir -p ${DEST_DIR}
+mv ${SRC_DIR} ${DEST_DIR}
+find ./pkg/client -type f -exec sed -i 's/azure/Azure/g' {} \; # rewrite bad imports to use Azure
